83da5690a05e7f719d7af4b795e5fb92b4c60453
bdenesh123 <drajan@wvb.com>
2025-11-26 16:19:52 +0800

    quick fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "../sections/Header";
-import JobSearch from "../sections/jobSearch";
 import Companies from "../sections/Companies";
 import LatestJobs from "../sections/LatestJobs";
 import Footer from "../sections/Footer";
+import JobSearch from "../sections/jobSearch";
 
 const Home = () => {
   return (
